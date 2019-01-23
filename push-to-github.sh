53610584a5e439bf5ba4d1c080492a19a8489b2f
panfeng <312887499@qq.com>
2019-01-23 21:21:54 +0800

    2019 01 23 21:21 (AutoPush Script)

---
+++ push-to-github.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# 脚本：自动化提交
+
+# 更新子模块
+git submodule foreach git pull
+
+# 日志信息
+Date=`date "+ %Y %m %d %H:%M"`
+
+# 标记当前路径
+DIR=`pwd`
+echo "当前目录：${DIR}"
+
+if [[ $? -ne 0 ]]
+then
+    echo "Please use 'sh $0'."
+    exit 1;
+fi
+
+msg="${Date} (AutoPush Script)"
+# 可以考虑设置一个提交参数
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+# 提交
+git add --all && git commit -m "${msg}"
+
+echo "正在 push"
+git push -u origin master
+
+echo "Down!"
